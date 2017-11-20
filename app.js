7c1e4ac7c9ab80412eebd3b6ed2e7d7aed7a0bee
psphanindra <psphanindra@gmail.com>
2017-11-20 18:55:12 +0530

    second commit

--- app.js
+++ app.js
@@ -1,46 +1,30 @@
 
-$(document).ready(function() {
-	$('#submit').click(function() {
-		var uname= $('#uname').val();
-		var pwd= $('#pwd').val();
-		alert("uerename is "+uname);
-		alert("pasword  is "+pwd);
-
-		$.ajax({
-			type: 'GET ',
-			dataType: ' json',
-			url: 'users.json',
-			success: function (data) {
-				alert("success from inside");
-				$.each(function(key,value) {
-					alert(value.username);
-					if (value.username!= " " && value.password !=" ") {
-						if (value.username!= uname && value.password != pwd){
-							/*window.load("feed.html");*/
-							alert("password correct");
-							return false;
-						}
-					}
-					
-				});
-			},
-			error:function () {
-				
-			}
-
-		})
-		
-		
-	});
-	
-});
-
-
-
-/*
 function login() {
 	var uname= document.getElementById("uname").value;
 	var pwd= document.getElementById("pwd").value;
 	
 
-}*/
+	var logRequest= new XMLHttpRequest();
+	logRequest.open("GET","users.json",true);
+	logRequest.onload = function () {
+		var userdata= JSON.parse(logRequest.response);
+		/*console.log(userdata);
+		var dat=JSON.stringify(userdata);
+		console.log(dat);*/
+		for(var i=0;i<userdata.length;i++){
+			if(userdata[i].username == uname && userdata[i].password == pwd){
+				window.location = "wall.html";
+				break;
+
+			}
+			
+		}
+	};
+	logRequest.send();
+
+}
+
+
+
+
+
