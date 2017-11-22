d1122cc5bd3279bfcf2c6fda63d1bb704518d5ec
psphanindra <31414714+psphanindra@users.noreply.github.com>
2017-11-22 12:51:29 +0530

    Update wall.js

--- wall.js
+++ wall.js
@@ -1,15 +1,16 @@
 
+
 "use strict";
 $(document).ready(function() {
 	var text_max = 240;
-$('#count_message').html(text_max + ' remaining');
+  $('#count_message').html(text_max + ' remaining');
 
-$('#text').keyup(function() {
+  $('#text').keyup(function() {
   var text_length = $('#text').val().length;
   var text_remaining = text_max - text_length;
  
   $('#count_message').html(text_remaining + ' remaining');
-  	 if(text_length > 240 ){
+  	 if(text_length > text_max ){
   		$('#submit-btn').attr('disabled', 'disabled');
   		$('#count_message').css('color', 'red');
   		}
@@ -17,10 +18,29 @@ $('#text').keyup(function() {
   		$('#submit-btn').removeAttr('disabled', 'disabled');
   		$('#count_message').css('color', 'black');
   	}
-});
-});
-	
-	
+  });
 
-	
+  
 
+          var likecounter =$('#like-counter').html();
+             /*alert(likecounter);*/
+
+          /*alert(likecounter);*/
+      $("#likebtn").click(function() {
+        likecounter++;
+        $("#like-counter").html(likecounter);
+
+  });
+   $('#create-comment').hide(); 
+   //$('#create-comment').show();
+   $('#cmtbtn').click(function() {
+    //alert("yupp..");
+    $('#create-comment').toggle(1000);
+
+    
+ 
+  });
+
+
+});
+	
